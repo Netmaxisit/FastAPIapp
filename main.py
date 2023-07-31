3227a66f47b30f6c8d801972a5210afae62c6523
Netmaxisit <netmaxisit@gmail.com>
2023-08-01 00:33:24 +0100

    Update main.py
    added database connection

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI, HTTPException
 from pytube import YouTube
 from youtube_transcript_api import YouTubeTranscriptApi
+import asyncpg
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+   try:
+        # Call the startup event from within the route (not recommended)
+        await app.startup()  # This will trigger the startup event and connect to the database
+
+        # Your actual route logic here
+        return {"Hello": "World!"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 def get_youtube_data(video_url):
     try:
@@ -28,4 +36,17 @@ async def extract_youtube_data(video_url: str):
         "text_transcript": text_transcript
     }
 
+async def connect_to_database():
+    connection_uri = f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DATABASE}"
+    return await asyncpg.connect(dsn=connection_uri)
 
+
+@app.on_event("startup")
+async def startup_event():
+    app.state.db = await connect_to_database()
+    print("Connected to PostgreSQL database!")
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await app.state.db.close()
